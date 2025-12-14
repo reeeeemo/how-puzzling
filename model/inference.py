6cd8e060c7923a992e55bc74888980b42b8e25c1
Robert Oxley <robert@oxley.ca>
2025-12-13 23:21:22 -0500

    fixed inference for test images

--- model/inference.py
+++ model/inference.py
@@ -2,21 +2,26 @@ from ultralytics import YOLO
 from pathlib import Path
 import cv2
 import numpy as np
-from utils.masks import clean_masks
 from dataset.dataset import PuzzleDataset
-import random
-import torch
+
 
 def main():
-    model = YOLO(str(Path("model") / "model_outputs" / "train3" /"weights" / "best.pt"))
+    model_dir = Path(__file__).resolve().parent
+    model = YOLO(model_dir / "model_outputs" / "train3" /"weights" / "best.pt")
     project_path = Path(__file__).resolve().parent.parent
     root_dir = project_path / "dataset" / "data" / "jigsaw_puzzle"
     dataset = PuzzleDataset(root_dir=root_dir, splits=["test"], extension="jpg")
     
     all_test_images = [img for img, _ in dataset]
     for image in all_test_images:
+        new_img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
         results = model(image)[0]
-        cv2.imshow("yolo output", results.plot())
+        
+        for c in results.masks.xy:
+            contour_np = np.array(c, dtype=np.int32)
+            cv2.drawContours(new_img, [contour_np], -1, (0,255,0), thickness=3)
+            
+        cv2.imshow("yolo output (cleaned)", new_img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
             
