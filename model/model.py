57cf29fcf3ca010e65ff43376c646dae85e16a24
Robert Oxley <robert@oxley.ca>
2025-12-17 23:04:42 -0500

    fixed similarity matching and added view of puzzle piece similarity

--- model/model.py
+++ model/model.py
@@ -13,6 +13,8 @@ class PuzzleImageModel(nn.Module):
         Attributes:
         model: pretrained YOLO model to segment images with
         device: whether device uses cuda or cpu
+        similarity_processor: DINOv3 image encoder's processor
+        similarity_model: DINOv3 image encoder
     """
     def __init__(self, model_name: str = "best.pt", device: str = "cpu"):
         super().__init__()
@@ -32,26 +34,44 @@ class PuzzleImageModel(nn.Module):
         """
             Forward pass of the model
             Args:
-                img_paths: list of numpy arrays
+                imgs: list of numpy arrays
             Returns:
-                Cosine similarities of each detected object relative to the current image
+                tuple of cosine similarities of each detected object,
+                xyxy coords of boxes cropped relative to segmented mask
         """
         results = self.model(imgs, verbose=False)
-        cropped, img_indices = self.crop_images(results, imgs)
+        cropped, boxes_per_image = self.crop_images(results, imgs)
         
         similarities = self.compute_similarities(cropped)
-        return similarities
+        return similarities, boxes_per_image
 
-    def compute_similarities(self, imgs):
+    def compute_embeddings(self, imgs):
+        """
+            Compute embeddings of an image using the encoder
+            Args:
+                imgs: list of numpy arrays
+        """
         with torch.no_grad():
             inputs = self.similarity_processor(images=imgs, return_tensors="pt").to(self.device)
             outputs = self.similarity_model(**inputs)
-            image_feats = outputs.last_hidden_state[:, 0, :] # extract cls token embedding
-            image_feats = nn.functional.normalize(image_feats, dim=1)
+            
+        return outputs
+    
+    def compute_similarities(self, imgs):
+        """
+            Compute a similarity matrix of multiple images using cosine similarity
+            Args:
+                imgs: list of numpy arrays        
+        """
+        outputs = self.compute_embeddings(imgs)
+        image_feats = outputs.last_hidden_state[:, 0, :] # extract cls token embedding
+        image_feats = nn.functional.normalize(image_feats, dim=1)
         
         # cos sim is (a \dot b) / ||a|| x ||b||, but we normalize so magnitude is 1 which reduces to
         # a \dot b
-        sim_mat = torch.mm(image_feats, image_feats.T)            
+        sim_mat = torch.mm(image_feats, image_feats.T)
+        sim_mat = torch.clamp(sim_mat, 0.0, 1.0)
+        sim_mat.fill_diagonal_(1.0)          
         return sim_mat
     
     def crop_images(self, results, imgs):
@@ -61,10 +81,11 @@ class PuzzleImageModel(nn.Module):
                 results: segmentation masks from model
                 imgs: list of numpy arrays
             Returns:
-                tuple: tensor of batched cropped and images list of indices
+                tuple: tensor of batched cropped,
+                xyxy coords of cropped box
         """
         all_crops = []
-        image_indices = []
+        boxes_per_image = {i: [] for i in range(len(imgs))}
         
         for idx, (result, img) in enumerate(zip(results, imgs)):
             boxes = result.boxes.xyxy
@@ -83,31 +104,9 @@ class PuzzleImageModel(nn.Module):
                 crop = img[y1:y2, x1:x2]
                 crop_t = torch.from_numpy(cv2.resize(crop, (640,640))).permute(2,0,1).float() / 255.0
                 all_crops.append(crop_t)
-                image_indices.append(idx)
+                boxes_per_image[idx].append((x1, y1, x2, y2))
                 
         if not all_crops:
-            return torch.empty(0, 3, 640, 640)
+            return torch.empty(0, 3, 640, 640), boxes_per_image
                         
-        return torch.stack(all_crops), image_indices
-                
-    def forward_until_layer(self, imgs, layer: int):
-        """
-            Compute forward pass of the model until the specified layer
-            Args:
-                imgs: list of images to compute forward pass on
-                layer: layer to return embeddings from
-            Returns:
-                features: list of embeddings
-        """
-        features = []
-        def hook_fn(module, input, output):
-            features.append(output)
-            
-        target_layer = self.model.model.model[layer]
-        hook = target_layer.register_forward_hook(hook_fn)
-        
-        with torch.no_grad():
-            _ = self.model(imgs, verbose=False)
-        hook.remove()
-            
-        return features[0]
+        return torch.stack(all_crops), boxes_per_image
--- similarity.py
+++ similarity.py
@@ -3,36 +3,100 @@ import torch
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+from scipy.stats import rankdata
 
 from model.model import PuzzleImageModel
 from dataset.dataset import PuzzleDataset
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-def compute_similarites(project_path: Path):
-    model_path = project_path / "model" / "model_outputs" / "train3" / "weights" / "best.pt"
-    dataset_path = project_path / "dataset" / "data" / "jigsaw_puzzle"
+def compute_similarites(model_path: Path, dataset_path: Path, split: str):
+    """
+        Given a path to a dataset and model, 
+        get the similarity matrix of the requested split
+        Args:
+            model_path: path to pretrained segmentation model
+            dataset_path: path to dataset of images in YOLO format
+            split: split to take images from
+        Returns:
+            tuple of cosine similarity matrix,
+            all images from split that was segmented,
+            xyxy coords of boxes cropped relative to segmented masks
+    """
+
     model = PuzzleImageModel(model_name=str(model_path), device=DEVICE)
-    dataset = PuzzleDataset(root_dir=dataset_path, splits=["test"], extension="jpg")
+    dataset = PuzzleDataset(root_dir=dataset_path, splits=[split], extension="jpg")
     
     all_test_images = [
         cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
         for img, _ in dataset
     ]
-    similarities = model(all_test_images)
-    return similarities
+    similarities, boxes_per_image = model(all_test_images)
+    return similarities, all_test_images, boxes_per_image
 
 def main():
     project_path = Path(__file__).resolve().parent
     output = project_path / "output"
+    model_path = project_path / "model" / "model_outputs" / "train3" / "weights" / "best.pt"
+    dataset_path = project_path / "dataset" / "data" / "jigsaw_puzzle"
     output.mkdir(exist_ok=True, parents=True)
-    sims = compute_similarites(project_path)
-    print(sims)
+     
     
-    #plt.imshow(sims, cmap="viridis")
-    #plt.title("Piece-to-piece cosine similarity")
-    #plt.savefig(str(output / "sim_heatmap.png"), dpi=300)
-    #plt.close()
+    sims, images, boxes_per_image = compute_similarites(model_path, dataset_path, "test")
+    
+    for idx, boxes in boxes_per_image.items():        
+        for i in range(len(sims)):
+            img = images[idx].copy()
+            sim_col = sims[:, i]
+            
+            # mask to exclude diagonal elements, then compute ranking
+            mask = np.ones(len(sim_col), dtype=bool)
+            mask[i] = False
+            
+            masked_sims = sim_col[mask]
+            ranked_sim = rankdata(masked_sims, method="ordinal") - 1 # 0 indexxed
+            normalized_ranks = ranked_sim / (len(ranked_sim) - 1) if len(ranked_sim) > 0 else ranked_sim
+            
+            # reconstruct with masked value, but cur piece == -1           
+            full_ranks = np.zeros(len(sim_col))
+            full_ranks[mask] = normalized_ranks
+            full_ranks[i] = -1
+            
+            for j, box in enumerate(boxes):
+                x1, y1, x2, y2 = box
+                
+                if j == i: # the piece we're comparing
+                    cv2.rectangle(img, (x1,y1), (x2,y2), (255, 255, 255), 2)
+                else:
+                    color = int(full_ranks[j] * 255)
+                    cv2.rectangle(img, (x1, y1), (x2,y2), (0, color, 255-color), 2)
+                
+            cv2.imshow(f"similarity results for piece {i}", img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            
+        
+    return
+    
+    """
+    for img_idx, piece_indices in pieces_by_image.items():
+        img = images[img_idx].copy()
+        boxes = boxes_per_image[img_idx]
+        
+        sub_sim = sims[np.ix_(piece_indices, piece_indices)]
+        
+        for local_idx, (piece_idx, box) in enumerate(zip(piece_indices, boxes)):
+            x1, y1, x2, y2 = box
+            
+            avg_sim = sub_sim[local_idx].mean().item()
+            color = int(avg_sim * 255)
+            cv2.rectangle(img, (x1,y1), (x2,y2), (0, color, 255-color), 2)
+            
+        cv2.imshow(f"annotated_img_{img_idx}", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
+       """ 
     
 if __name__ == "__main__":
     main()
